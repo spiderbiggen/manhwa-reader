eb0dd19df637a8a0d11b7be61e32631971351ee2
Stefan Breetveld <spiderbiggen@gmail.com>
2024-01-02 23:30:33 +0100

    improve navigation consistency

--- presentation/src/main/java/com/spiderbiggen/manhwa/presentation/ui/main/MainContent.kt
+++ presentation/src/main/java/com/spiderbiggen/manhwa/presentation/ui/main/MainContent.kt
@@ -3,6 +3,8 @@ package com.spiderbiggen.manhwa.presentation.ui.main
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -22,10 +24,16 @@ fun MainContent() {
     ManhwaReaderTheme {
         Surface {
             NavHost(navController = navController, startDestination = "overview") {
-                composable("overview") {
+                composable("overview") { backStackEntry ->
                     val viewModel: ManhwaViewModel = hiltViewModel()
                     ManhwaOverview(
-                        navigateToManhwa = { navController.navigate("manhwa/${it}") },
+                        navigateToManhwa = {
+                            if (backStackEntry.lifecycleIsResumed()) {
+                                navController.navigate("manhwa/${it}") {
+                                    restoreState = true
+                                }
+                            }
+                        },
                         viewModel = viewModel
                     )
                 }
@@ -39,7 +47,13 @@ fun MainContent() {
                     val manhwaId = checkNotNull(backStackEntry.arguments?.getString("manhwaId"))
                     ChapterOverview(
                         onBackClick = { navController.popBackStack() },
-                        navigateToChapter = { navController.navigate("manhwa/$manhwaId/chapter/$it") },
+                        navigateToChapter = {
+                            if (backStackEntry.lifecycleIsResumed()) {
+                                navController.navigate("manhwa/$manhwaId/chapter/$it") {
+                                    restoreState = true
+                                }
+                            }
+                        },
                         viewModel = viewModel
                     )
                 }
@@ -50,13 +64,31 @@ fun MainContent() {
                         navArgument("chapterId") { type = NavType.StringType }
                     )
                 ) { backStackEntry ->
-                    val viewModel: ImagesViewModel = hiltViewModel()
                     val manhwaId = checkNotNull(backStackEntry.arguments?.getString("manhwaId"))
+                    val viewModel: ImagesViewModel = hiltViewModel()
                     ImagesOverview(
                         onBackClick = {
-                            navController.popBackStack(route = "manhwa/$manhwaId", false)
+                            if (backStackEntry.lifecycleIsResumed()) {
+                                navController.navigate("manhwa/$manhwaId") {
+                                    restoreState = true
+                                    popUpTo("manhwa/$manhwaId") {
+                                        inclusive = true
+                                        saveState = true
+                                    }
+                                }
+                            }
                         },
-                        toChapterClicked = { navController.navigate("manhwa/$manhwaId/chapter/$it") },
+                        toChapterClicked = {
+                            if (backStackEntry.lifecycleIsResumed()) {
+                                navController.navigate("manhwa/$manhwaId/chapter/$it") {
+                                    popUpTo("manhwa/$manhwaId") {
+                                        inclusive = true
+                                        saveState = true
+                                    }
+                                    restoreState = true
+                                }
+                            }
+                        },
                         viewModel = viewModel
                     )
                 }
@@ -64,3 +96,12 @@ fun MainContent() {
         }
     }
 }
+
+/**
+ * If the lifecycle is not resumed it means this NavBackStackEntry already processed a nav event.
+ *
+ * This is used to de-duplicate navigation events.
+ */
+private fun NavBackStackEntry.lifecycleIsResumed() =
+    this.lifecycle.currentState == Lifecycle.State.RESUMED
+
