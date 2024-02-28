21e4ae1b694874a0f9da2c44666628c5976533ce
Stefan Breetveld <spiderbiggen@gmail.com>
2024-02-29 00:25:46 +0100

    configure spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,3 +28,27 @@ tasks.withType<DependencyUpdatesTask> {
         isNonStable(candidate.version) && !isNonStable(currentVersion)
     }
 }
+
+spotless {
+    // optional: limit format enforcement to just the files changed by this feature branch
+    ratchetFrom("origin/master")
+
+    format("misc") {
+        // define the files to apply `misc` to
+        target(".gitattributes", ".gitignore")
+
+        // define the steps to apply to those files
+        trimTrailingWhitespace()
+        indentWithTabs()
+        endWithNewline()
+    }
+    kotlin {
+        // by default the target is every '.kt' and '.kts` file in the java sourcesets
+        ktlint(libs.versions.ktlint.get())   // has its own section below
+//        licenseHeader("/* (C)\$YEAR */") // or licenseHeaderFile
+    }
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktlint(libs.versions.ktlint.get())
+    }
+}
