ea230f797795d75b4a7d7a4369d43b757ccf234b
Stefan Breetveld <spiderbiggen@gmail.com>
2024-05-22 01:06:29 +0200

    Release 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.spiderbiggen.manhwa"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.1.3"
+        versionCode = 7
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
