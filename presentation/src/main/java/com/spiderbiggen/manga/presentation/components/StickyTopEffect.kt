3f90f9a73826db0824598ba118882a14aa53bd27
Stefan Breetveld <spiderbiggen@gmail.com>
2024-05-28 01:32:26 +0200

    fix 🤔 StickyTopEffect

--- presentation/src/main/java/com/spiderbiggen/manga/presentation/components/StickyTopEffect.kt
+++ presentation/src/main/java/com/spiderbiggen/manga/presentation/components/StickyTopEffect.kt
@@ -1,34 +1,40 @@
 package com.spiderbiggen.manga.presentation.components
 
-import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.interaction.Interaction
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshotFlow
 import kotlinx.collections.immutable.ImmutableCollection
 
 @Composable
 fun <T : Any?> StickyTopEffect(items: ImmutableCollection<T>, state: LazyListState) {
-    var notManuallyScrolled by remember { mutableStateOf(state.firstVisibleItemIndex == 0) }
-    val isPressed = state.interactionSource.collectIsPressedAsState()
-    LaunchedEffect(state) {
-            snapshotFlow { state.firstVisibleItemIndex }
-                .collect {
-                    if (it == 0) {
-                        notManuallyScrolled = true
-                    } else if (isPressed.value) {
-                        notManuallyScrolled = false
-                    }
-                }
+    var manuallyScrolled by remember { mutableStateOf(state.firstVisibleItemIndex == 0) }
+    val canScrollBackwards = state.canScrollBackward
+    val isDragged by state.interactionSource.collectIsDraggedAsState()
+    DisposableEffect(canScrollBackwards, isDragged) {
+        if (!canScrollBackwards) {
+            manuallyScrolled = false
+        } else if (isDragged) {
+            manuallyScrolled = true
+        }
+        onDispose { }
     }
     LaunchedEffect(items, state) {
-        if (notManuallyScrolled) {
+        if (!manuallyScrolled) {
             // scroll to top to ensure latest added element gets visible
             state.animateScrollToItem(0)
         }
     }
 }
+
+private data class ScrollState(
+    val index: Int,
+    val offset: Int,
+    val interaction: Interaction,
+)
