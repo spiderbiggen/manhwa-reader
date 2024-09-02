3f702c18adda2aa4bb4a73ca25dd792f0560a592
Stefan Breetveld <spiderbiggen@gmail.com>
2024-09-03 00:45:04 +0200

    chore(release): bump version to 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.spiderbiggen.manga"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "1.5.0"
+        versionCode = 19
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
