ca50ac084651c71e87eb077c59cb1feea638a57e
Stefan Breetveld <spiderbiggen@gmail.com>
2024-10-24 00:54:48 +0200

    feat: improve nested navigation

--- presentation/src/main/java/com/spiderbiggen/manga/presentation/ui/main/MainContent.kt
+++ presentation/src/main/java/com/spiderbiggen/manga/presentation/ui/main/MainContent.kt
@@ -1,16 +1,9 @@
-@file:OptIn(ExperimentalSharedTransitionApi::class)
-
 package com.spiderbiggen.manga.presentation.ui.main
 
 import android.os.Bundle
-import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
-import androidx.compose.animation.SharedTransitionLayout
-import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.compositionLocalOf
 import androidx.navigation.NavController.OnDestinationChangedListener
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -23,26 +16,17 @@ import com.spiderbiggen.manga.presentation.theme.MangaReaderTheme
 import com.spiderbiggen.manga.presentation.ui.manga.host.mangaNavigation
 import com.spiderbiggen.manga.presentation.ui.manga.model.MangaRoutes
 
-val LocalNavAnimatedVisibilityScope = compositionLocalOf<AnimatedVisibilityScope?> { null }
-val LocalSharedTransitionScope = compositionLocalOf<SharedTransitionScope?> { null }
-
 @Composable
 fun MainContent(coverImageLoader: ImageLoader, chapterImageLoader: ImageLoader) {
     val navController = rememberNavController()
 
     MangaReaderTheme {
         NavigationTrackingSideEffect(navController)
-        SharedTransitionLayout {
-            CompositionLocalProvider(
-                LocalSharedTransitionScope provides this,
-            ) {
-                NavHost(
-                    navController = navController,
-                    startDestination = MangaRoutes.Host,
-                ) {
-                    mangaNavigation(navController, coverImageLoader, chapterImageLoader)
-                }
-            }
+        NavHost(
+            navController = navController,
+            startDestination = MangaRoutes.Host,
+        ) {
+            mangaNavigation(navController, coverImageLoader, chapterImageLoader)
         }
     }
 }
@@ -57,7 +41,7 @@ private fun NavigationTrackingSideEffect(navController: NavHostController) {
             val bundle = (arguments?.deepCopy() ?: Bundle()).apply {
                 val extraKeys = keySet() - destination.arguments.keys
                 extraKeys.forEach { key -> remove(key) }
-                putString(FirebaseAnalytics.Event.SCREEN_VIEW, destination.route?.takeLast(100))
+                putString(FirebaseAnalytics.Param.SCREEN_NAME, destination.route?.takeLast(100))
             }
 
             Firebase.analytics.logEvent(
