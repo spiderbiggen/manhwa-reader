51c6be60783fa6e47b7d7d2911ec03c7d05b6d69
Stefan Breetveld <spiderbiggen@gmail.com>
2024-10-29 19:52:31 +0100

    chore(release): 1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.spiderbiggen.manga"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.7.0"
+        versionCode = 24
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
