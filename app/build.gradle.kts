e43f3ada0e18274d243792c12b163ad9be91c6b5
Stefan Breetveld <spiderbiggen@gmail.com>
2025-02-18 20:06:27 +0100

    chore(release): v1.7.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,13 +24,10 @@ android {
         applicationId = "com.spiderbiggen.manga"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.7.7"
+        versionCode = 31
+        versionName = "1.7.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     signingConfigs {
