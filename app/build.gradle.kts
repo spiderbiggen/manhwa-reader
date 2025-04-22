978149dca25b125b325d1f1734300c89975fcb05
Stefan Breetveld <spiderbiggen@gmail.com>
2025-04-22 23:41:24 +0200

    chore(release): 1.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.spiderbiggen.manga"
         minSdk = 26
         targetSdk = 35
-        versionCode = 35
-        versionName = "1.9.0"
+        versionCode = 36
+        versionName = "1.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
