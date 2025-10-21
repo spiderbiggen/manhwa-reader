fff19bc5b2e104ab85c699bdff31ef5ed0623bc6
Stefan Breetveld <spiderbiggen@gmail.com>
2025-10-21 19:54:53 +0200

    chore: clean up unused imports

--- presentation/src/main/java/com/spiderbiggen/manga/presentation/theme/Theme.kt
+++ presentation/src/main/java/com/spiderbiggen/manga/presentation/theme/Theme.kt
@@ -6,9 +6,6 @@ import androidx.compose.material3.MotionScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.materialkolor.DynamicMaterialExpressiveTheme
-import com.materialkolor.DynamicMaterialTheme
-import com.materialkolor.PaletteStyle
-import com.materialkolor.dynamiccolor.ColorSpec
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
