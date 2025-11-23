0c5b5c51e6fb059007cb8ba45b776b65efa4bf84
Stefan Breetveld <spiderbiggen@gmail.com>
2025-11-23 16:18:25 +0100

    chore: fix functional interface

--- domain/src/main/java/com/spiderbiggen/manga/domain/usecase/auth/Logout.kt
+++ domain/src/main/java/com/spiderbiggen/manga/domain/usecase/auth/Logout.kt
@@ -3,6 +3,6 @@ package com.spiderbiggen.manga.domain.usecase.auth
 import com.spiderbiggen.manga.domain.model.AppError
 import com.spiderbiggen.manga.domain.model.Either
 
-interface Logout {
+fun interface Logout {
     suspend operator fun invoke(): Either<Unit, AppError>
 }
