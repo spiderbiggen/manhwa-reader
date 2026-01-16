3b04f20b833f52a8ab96ce2f13e2cb5b02d4aac6
Stefan Breetveld <spiderbiggen@gmail.com>
2026-01-16 12:43:57 +0100

    chore(deps): remove Kotlin Android plugin references

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlinX.compose)
     alias(libs.plugins.google.ksp)
     alias(libs.plugins.google.gms.google.services)
