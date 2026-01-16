e2bd6b7350ccc012419ae2f8c272add98d3a8aba
Stefan Breetveld <spiderbiggen@gmail.com>
2026-01-16 15:15:14 +0100

    chore: re-enable ratchetFrom for format enforcement in spotless configuration

--- buildSrc/src/main/kotlin/manga.spotless.gradle.kts
+++ buildSrc/src/main/kotlin/manga.spotless.gradle.kts
@@ -15,7 +15,7 @@ if (project == rootProject) {
 } else {
     spotless {
         // optional: limit format enforcement to just the files changed by this feature branch
-        // ratchetFrom("origin/master")
+        ratchetFrom("origin/master")
 
         format("misc") {
             // define the files to apply `misc` to
