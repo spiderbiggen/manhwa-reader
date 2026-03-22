c04f7eb39bad25d542915f899824f7927fb2ec04
Stefan Breetveld <spiderbiggen@gmail.com>
2026-03-23 00:30:24 +0100

    chore(release): 1.26.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.spiderbiggen.manga"
         minSdk = 26
         targetSdk = 36
-        versionCode = 77
-        versionName = "1.26.3"
+        versionCode = 78
+        versionName = "1.26.4"
     }
 
     signingConfigs {
