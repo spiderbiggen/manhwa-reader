feef70ddcb97edd43b147fd65d2ef3f23482ac76
Stefan Breetveld <stefan.breetveld@wearetriple.com>
2026-05-21 18:22:43 +0000

    feat(theme): apply Cosmic Pop brand theme, remove dynamic color
    - Replace muted default palette in Color.kt with Cosmic Pop colors
      (vibrant purple primary, hot pink secondary, warm amber tertiary)
    - Drop unused medium/high contrast color scheme variants
    - Fix Theme.kt: wire up lightScheme/darkScheme which were previously
      ignored in favor of blank darkColorScheme()/expressiveLightColorScheme()
    - Remove DynamicColors.applyToActivitiesIfAvailable from MangaApp
    - Delete values-v34/values-night-v34 color overrides that referenced
      m3_sys_color dynamic tokens
    - Update launcher_background/foreground_accent to Cosmic Pop primary

--- app/src/main/java/com/spiderbiggen/manga/MangaApp.kt
+++ app/src/main/java/com/spiderbiggen/manga/MangaApp.kt
@@ -3,7 +3,6 @@ package com.spiderbiggen.manga
 import android.app.Application
 import androidx.compose.runtime.Composer
 import androidx.compose.runtime.tooling.ComposeStackTraceMode
-import com.google.android.material.color.DynamicColors
 import com.spiderbiggen.manga.data.di.dataModule
 import com.spiderbiggen.manga.presentation.di.presentationModule
 import org.koin.android.ext.koin.androidContext
@@ -13,8 +12,6 @@ import org.koin.core.context.startKoin
 class MangaApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        DynamicColors.applyToActivitiesIfAvailable(this)
-
         Composer.setDiagnosticStackTraceMode(ComposeStackTraceMode.Auto)
 
         startKoin {
