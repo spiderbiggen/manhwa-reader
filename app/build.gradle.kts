c162ff7398fad8cc146e87f88978e5ba3e707aa5
Stefan Breetveld <spiderbiggen@gmail.com>
2026-09-11 02:14:51 +0200

    chore(release): bump version to 1.32.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.spiderbiggen.manga"
         minSdk = 26
         targetSdk = 37
-        versionCode = 85
-        versionName = "1.32.0"
+        versionCode = 86
+        versionName = "1.32.1"
     }
 
     signingConfigs {
